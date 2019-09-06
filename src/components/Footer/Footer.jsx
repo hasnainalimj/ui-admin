7e7f0f304346affa688dd52d73afd67674425d67
hasnainalimj <mjhasnain15@gmail.com>
2019-09-06 14:20:53 +0500

    initial commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { Container } from "reactstrap";
+// used for making the prop types of this component
+import PropTypes from "prop-types";
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer
+        className={"footer" + (this.props.default ? " footer-default" : "")}
+      >
+        <Container fluid={this.props.fluid ? true : false}>
+          <nav>
+            <ul>
+              <li>
+                <a href="#" target="_blank">Company Name</a>
+              </li>
+              <li>
+                <a href="#" target="_blank">About Us</a>
+              </li>
+              <li>
+                <a href="#" target="_blank">Blog</a>
+              </li>
+            </ul>
+          </nav>
+          <div className="copyright">
+            {/* &copy; {1900 + new Date().getYear()}, Designed by{" "}
+            <a
+              href="#"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Invision
+            </a>
+            . Coded by{" "}
+            <a
+              href="#"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Admin UI
+            </a>
+            . */}
+          </div>
+        </Container>
+      </footer>
+    );
+  }
+}
+
+Footer.propTypes = {
+  default: PropTypes.bool,
+  fluid: PropTypes.bool
+};
+
+export default Footer;
